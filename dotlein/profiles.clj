8adbfc8b3a588c919d12078da3448589a03d6c1b
Teemu Patja <tp@iki.fi>
2015-05-01 23:40:12 +0300

    * add lein-test-refresh

--- dotlein/profiles.clj
+++ dotlein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.8.1"]
-            [refactor-nrepl "1.0.3"]]
+ {:plugins [[cider/cider-nrepl "0.8.2"]
+            [refactor-nrepl "1.0.3"]
+            [com.jakemccrary/lein-test-refresh "0.9.0"]]
   :dependencies [[spyscope "0.1.4"]
                   [org.clojure/tools.namespace "0.2.4"]
                   [leiningen #=(leiningen.core.main/leiningen-version)]
@@ -11,13 +12,12 @@
                (require '[vinyasa.inject :as inject])
                (require 'io.aviso.repl)
                (inject/in ;; the default injected namespace is `.` 
-
                 ;; note that `:refer, :all and :exclude can be used
                 [vinyasa.inject :refer [inject [in inject-in]]]  
                 [vinyasa.lein :exclude [*project*]]  
                 
                 ;; imports all functions in vinyasa.pull
-                [vinyasa.pull :all]      
+                [vinyasa.pull :all]
                 
                 ;; same as [cemerick.pomegranate 
                 ;;           :refer [add-classpath get-classpath resources]]
@@ -32,4 +32,4 @@
                 [clojure.pprint pprint]
                 [clojure.java.shell sh]
                 [clojure.pprint pprint pp]
-                [alex-and-georges.debug-repl debug-repl])] }}
+                [alex-and-georges.debug-repl debug-repl])]}}
