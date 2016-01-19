86bf314a45a4caba4b79174f3127cdce2ca3f2e3
Teemu Patja <tp@iki.fi>
2016-01-19 10:50:45 +0200

    Use spaces for indentation in python

--- dotemacs.d/customizations/setup-python.el
+++ dotemacs.d/customizations/setup-python.el
@@ -6,7 +6,7 @@
 
 (add-hook 'python-mode-hook
           (lambda ()
-            (setq indent-tabs-mode t)
-            (setq tab-width 4)
+            (setq indent-tabs-mode nil)
+            (setq tab-width 2)
             (add-to-list 'write-file-functions 'delete-trailing-whitespace)
             (eldoc-mode 1) t))
