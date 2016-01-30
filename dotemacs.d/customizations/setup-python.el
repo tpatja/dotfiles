7ff11059a9c8849a2c21a23f028efabbb6ffb43b
Teemu Patja <tp@iki.fi>
2016-01-30 11:10:55 +0200

    No eldoc mode in python mode

--- dotemacs.d/customizations/setup-python.el
+++ dotemacs.d/customizations/setup-python.el
@@ -1,6 +1,5 @@
 ;; Python
 
-(autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" .  python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
@@ -8,5 +7,4 @@
           (lambda ()
             (setq indent-tabs-mode nil)
             (setq tab-width 2)
-            (add-to-list 'write-file-functions 'delete-trailing-whitespace)
-            (eldoc-mode 1) t))
+            (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
