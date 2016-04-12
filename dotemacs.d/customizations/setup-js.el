b5c1e931123a5951c7006933d59602aeb61b2c77
Teemu Patja <tp@iki.fi>
2016-04-12 17:00:36 +0300

    Switch from js-mode to js2-mode

--- dotemacs.d/customizations/setup-js.el
+++ dotemacs.d/customizations/setup-js.el
@@ -1,9 +1,8 @@
 ;; javascript / html
-(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-hook 'js-mode-hook 'subword-mode)
 (add-hook 'html-mode-hook 'subword-mode)
-(setq js-indent-level 2)
 (eval-after-load "sgml-mode"
   '(progn
      (require 'tagedit)
