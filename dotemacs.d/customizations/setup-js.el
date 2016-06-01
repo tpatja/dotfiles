3dbc1012cd4f273623b8e8fa63f05576095f8517
Teemu Patja <tp@iki.fi>
2016-06-01 10:47:35 +0300

    Use web-mode for all js files

--- dotemacs.d/customizations/setup-js.el
+++ dotemacs.d/customizations/setup-js.el
@@ -1,5 +1,5 @@
 ;; javascript / html
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-hook 'js-mode-hook 'subword-mode)
 (add-hook 'html-mode-hook 'subword-mode)
