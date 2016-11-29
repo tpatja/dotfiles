33016bc9c8e8ebfb2c7e67fd331019434a2c98a0
Teemu Patja <tp@iki.fi>
2016-11-29 11:20:00 +0200

    Remove plugins from lein profiles.clj
    * keep lein plugin config per project

--- dotlein/profiles.clj
+++ dotlein/profiles.clj
@@ -1,33 +1,3 @@
 {:user
- {
-  :plugins [[cider/cider-nrepl "0.9.1"]
-            [refactor-nrepl "1.1.0"]
-            [com.jakemccrary/lein-test-refresh "0.11.0"]]
-  :dependencies [[spyscope "0.1.5"]
-                 [org.clojure/tools.namespace "0.2.11"]
-                 [leiningen #=(leiningen.core.main/leiningen-version)]
-                 [io.aviso/pretty "0.1.20"]
-                 [alembic "0.3.2"]
-                 [im.chit/vinyasa "0.4.2"]
-                 [org.clojars.gjahad/debug-repl "0.3.3"]]
-  :injections [(require 'spyscope.core)
-               (require '[vinyasa.inject :as inject])
-               (require 'io.aviso.repl)
-               (inject/in ;; the default injected namespace is `.` 
-                ;; note that `:refer, :all and :exclude can be used
-                [vinyasa.inject :refer [inject [in inject-in]]]  
-                [vinyasa.lein :exclude [*project*]]  
-                
-                ;; imports all functions in vinyasa.pull
-                [alembic.still [distill pull]]
-                
-                ;; inject into clojure.core 
-                clojure.core
-                [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
-                
-               ;; inject into clojure.core with prefix
-                clojure.core >
-                [clojure.pprint pprint]
-                [clojure.java.shell sh]
-                [clojure.pprint pprint pp]
-                [alex-and-georges.debug-repl debug-repl])]}}
+ {:plugins []
+  :android {:sdk-path "/usr/local/opt/android-sdk"}}}
