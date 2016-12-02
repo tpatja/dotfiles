983fc69ec4bf33de2c9a3149c089f79960d2d940
Teemu Patja <tp@iki.fi>
2016-12-02 23:14:50 +0200

    Emacs clojure config tweaks
    * cljr keybindings
    * elisp cleanup
    * remove unused things

--- dotemacs.d/customizations/setup-clojure.el
+++ dotemacs.d/customizations/setup-clojure.el
@@ -9,6 +9,9 @@
 ;; Java classes (e.g. JavaClassName)
 (add-hook 'clojure-mode-hook 'subword-mode)
 
+;; mandatory lambda symbol :)
+(add-hook 'clojure-mode-hook 'prettify-symbols-mode)
+
 ;; A little more syntax highlighting
 (require 'clojure-mode-extra-font-locking)
 
@@ -53,38 +56,18 @@
 ;; enable paredit in your REPL
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
+
+
 ;; Use clojure mode for other extensions
-(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
-
-
-;; key bindings
-;; these help me out with the way I usually develop web apps
-(defun cider-start-http-server ()
-  (interactive)
-  (cider-load-current-buffer)
-  (let ((ns (cider-current-ns)))
-    (cider-repl-set-ns ns)
-    (cider-interactive-eval (format "(println '(def server (%s/start))) (println 'server)" ns))
-    (cider-interactive-eval (format "(def server (%s/start)) (println server)" ns))))
-
-
-(defun cider-refresh ()
-  (interactive)
-  (cider-interactive-eval (format "(user/reset)")))
-
-(defun cider-user-ns ()
-  (interactive)
-  (cider-repl-set-ns "user"))
-
-(eval-after-load 'cider
-  '(progn
-     (define-key clojure-mode-map (kbd "C-c C-v") 'cider-start-http-server)
-     (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
-     (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
-     (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)))
+(dolist (pattern '("\\.edn$"
+                   "\\.boot$"
+                   "lein-env"))
+  (add-to-list 'auto-mode-alist (cons pattern 'clojure-mode)))
 
 ;; clj-refactor
 (cljr-add-keybindings-with-prefix "C-c C-m")
+
+(eval-after-load 'clj-refactor-mode
+  (define-key clj-refactor-map (kbd "C-c '") 'hydra-cljr-cljr-menu/body))
+
 (setq cljr-warn-on-eval nil)
