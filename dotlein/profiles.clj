cf8692c1a421b24122d3dc1c9484866ffc75a67b
Teemu Patja <tp@iki.fi>
2016-12-02 23:10:55 +0200

    Add useful lein plugins to user profiles.clj
    * lein ancient for checking outdated dependencies
    * linters and static analysis tools

--- dotlein/profiles.clj
+++ dotlein/profiles.clj
@@ -1,3 +1,7 @@
 {:user
- {:plugins []
+ {:plugins [[lein-ancient "0.6.10"]
+            [jonase/eastwood "0.2.3"]
+            [lein-bikeshed "0.4.1"]
+            [lein-cljfmt "0.5.6"]
+            [lein-kibit "0.1.3"]]
   :android {:sdk-path "/usr/local/opt/android-sdk"}}}
