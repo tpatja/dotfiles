f1c0ede8a6f681dec8f99748ff7b33f680354c99
Teemu Patja <tp@iki.fi>
2017-01-23 19:31:23 +0200

    Add yagni to global lein plugins

--- dotlein/profiles.clj
+++ dotlein/profiles.clj
@@ -3,5 +3,6 @@
             [jonase/eastwood "0.2.3"]
             [lein-bikeshed "0.4.1"]
             [lein-cljfmt "0.5.6"]
-            [lein-kibit "0.1.3"]]
+            [lein-kibit "0.1.3"]
+            [venantius/yagni "0.1.4"]]
   :android {:sdk-path "/usr/local/opt/android-sdk"}}}
