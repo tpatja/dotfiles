ba405f38c4099ed1f9b3a69eaf8f49df7ae8ac63
Teemu Patja <tp@iki.fi>
2019-11-19 17:47:43 +0200

    Updates to emacs + bash configs

--- dotemacs.d/customizations/setup-python.el
+++ dotemacs.d/customizations/setup-python.el
@@ -7,4 +7,25 @@
           (lambda ()
             (setq indent-tabs-mode nil)
             (setq tab-width 4)
-            (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+            (add-to-list 'write-file-functions 'delete-trailing-whitespace)
+            (local-set-key (kbd "C-M-<right>") 'hs-show-block)
+            (local-set-key (kbd "C-M-<left>")  'hs-hide-block)
+            (local-set-key (kbd "C-M-<up>")    'hs-hide-all)
+            (local-set-key (kbd "C-M-<down>")  'hs-show-all)
+            (elpy-mode t)))
+
+(require 'pydoc-info)
+
+(info-lookup-add-help
+ :mode 'python-mode
+ :parse-rule 'pydoc-info-python-symbol-at-point
+ :doc-spec
+ '(("(python)Index" pydoc-info-lookup-transform-entry)
+   ("(sphinx)Index" pydoc-info-lookup-transform-entry)))
+
+(require 'elpy-mode)
+
+(eval-after-load "elpy-mode"
+  '(progn
+    (define-key elpy-mode-map (kbd "M-<right>") nil)
+    (define-key elpy-mode-map (kbd "M-<left>") nil)))
