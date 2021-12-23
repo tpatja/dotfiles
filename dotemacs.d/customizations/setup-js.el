15e08e35814a7a781958aa8e5ffd95d0395b0e8a
Teemu Patja <tp@iki.fi>
2021-12-23 15:41:17 +0200

    Improve emacs config for web files

--- dotemacs.d/customizations/setup-js.el
+++ dotemacs.d/customizations/setup-js.el
@@ -9,21 +9,11 @@
      (tagedit-add-paredit-like-keybindings)
      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
 
-
-;; coffeescript
-(add-to-list 'auto-mode-alist '("\\.coffee.erb$" . coffee-mode))
-(add-hook 'coffee-mode-hook 'subword-mode)
-(add-hook 'coffee-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'coffee-mode-hook
-          (defun coffee-mode-newline-and-indent ()
-            (define-key coffee-mode-map "\C-j" 'coffee-newline-and-indent)
-            (setq coffee-cleanup-whitespace nil)))
-(custom-set-variables
- '(coffee-tab-width 4))
-
-;; http://emacs.stackexchange.com/a/20055
 (add-hook 'web-mode-hook
       (lambda ()
         (if (equal web-mode-content-type "javascript")
-            (web-mode-set-content-type "jsx")
-          (message "now set to: %s" web-mode-content-type))))
+            ;; http://emacs.stackexchange.com/a/20055
+            (web-mode-set-content-type "jsx"))
+        (setq web-mode-markup-indent-offset 2)
+        (setq web-mode-css-indent-offset 2)
+        (setq web-mode-code-indent-offset 2)))
